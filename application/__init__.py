09195647e156f74497c54070c353cc07874a29e2
Lenilson Nascimento <lenilson.100@gmail.com>
2017-07-05 20:43:58 -0300

    feat(services): Added support for swagger docs

--- application/__init__.py
+++ application/__init__.py
@@ -2,10 +2,20 @@
 import falcon
 from application.status_service import StatusService
 from application.user_service import UserService
+from application.spec_service import SpecService
+from falcon_swagger_ui import register_swaggerui_app
 
+# Defining documentation specs
+SWAGGER_URL = '/apidocs'
+API_URL = '/api_docs.json'
 # Starting falcon API
 app = falcon.API()
 
-#Registering services
+# Registering services
 app.add_route('/', StatusService())
 app.add_route('/user/{user_name}', UserService())
+app.add_route('/api_docs.json', SpecService())
+
+register_swaggerui_app(app, SWAGGER_URL, API_URL, config={
+    'supportedSubmitMethods': ['get'],
+})
--- application/base_service.py
+++ application/base_service.py
@@ -1,9 +1,17 @@
 """Base service"""
+import json
+import yaml
 from jinja2 import Environment, PackageLoader
 
 class BaseService(object):
     def __init__(self):
         self.env = Environment(loader=PackageLoader('application', 'templates'))
+        self.spec_env = Environment(loader=PackageLoader('application', 'specs'))
+
+    def render_specs(self):
+        template = self.spec_env.get_template('default.yml')
+        rendered = template.render()
+        return json.dumps(yaml.load(rendered))
 
     def render_template(self, name, **kwargs):
         """Render the given @name template and gives args to it"""
